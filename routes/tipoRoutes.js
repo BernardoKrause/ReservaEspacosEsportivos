152b0b04976d89462d751cf39e9cda6c7dd99f7e
Caio <caioFS23@hotmail.com>
2024-09-24 21:00:18 -0300

    remove: tipo por nome

--- routes/tipoRoutes.js
+++ routes/tipoRoutes.js
@@ -7,6 +7,5 @@ router
     .get('/api/tipos', tipoController.getAllTipos)
     .get('/api/tipos/:id', tipoController.listTipoById)
     .get('/api/tipos/espaco/:idEspaco', tipoController.listTipoByidEspaco)
-    .get('/api/tipos/nomes', tipoController.listTipoNome)
 
 module.exports = router;
